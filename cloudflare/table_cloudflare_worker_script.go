ccccb6ac8f595706bec9d6752151f5e49ff680a5
LalitTurbot <lalit@turbot.com>
2021-10-01 19:39:28 +0530

    update

--- cloudflare/table_cloudflare_worker_script.go
+++ cloudflare/table_cloudflare_worker_script.go
@@ -14,18 +14,13 @@ func tableCloudflareWorkerScript(ctx context.Context) *plugin.Table {
 		List: &plugin.ListConfig{
 			Hydrate: listWorkers,
 		},
-		// Get: &plugin.GetConfig{
-		// 	KeyColumns:        plugin.SingleColumn("id"),
-		// 	ShouldIgnoreError: isNotFoundError([]string{"Invalid zone identifier"}),
-		// 	Hydrate:           getZone,
-		// },
 		Columns: []*plugin.Column{
 			// Top columns
-			{Name: "id", Type: proto.ColumnType_STRING, Description: "Zone identifier tag."},
+			{Name: "id", Type: proto.ColumnType_STRING, Description: "Script identifier."},
 			{Name: "etag", Type: proto.ColumnType_STRING, Description: "Hashed script content, can be used in a If-None-Match header when updating."},
 			{Name: "size", Type: proto.ColumnType_INT, Description: "Size of the script, in bytes."},
-			{Name: "created_on", Type: proto.ColumnType_TIMESTAMP, Description: "The domain name."},
-			{Name: "modified_on", Type: proto.ColumnType_TIMESTAMP, Description: "When the script was last modified."},
+			{Name: "created_on", Type: proto.ColumnType_TIMESTAMP, Description: "Timestamp when the script was created."},
+			{Name: "modified_on", Type: proto.ColumnType_TIMESTAMP, Description: "Timestamp when the script was last modified."},
 		},
 	}
 }
@@ -34,78 +29,16 @@ func listWorkers(ctx context.Context, d *plugin.QueryData, _ *plugin.HydrateData
 	logger := plugin.Logger(ctx)
 	conn, err := connect(ctx, d)
 	if err != nil {
+		logger.Error("listWorkers", "connection error", err)
 		return nil, err
 	}
 	resp, err := conn.ListWorkerScripts(ctx)
 	if err != nil {
+		logger.Error("listWorkers", "ListWorkerScripts api error", err)
 		return nil, err
 	}
-	for index, resource := range resp.WorkerList {
-		logger.Info("listWorkers", index, resource)
+	for _, resource := range resp.WorkerList {
 		d.StreamListItem(ctx, resource)
 	}
 	return nil, nil
 }
-
-// func getZone(ctx context.Context, d *plugin.QueryData, _ *plugin.HydrateData) (interface{}, error) {
-// 	conn, err := connect(ctx, d)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	quals := d.KeyColumnQuals
-// 	zoneID := quals["id"].GetStringValue()
-// 	item, err := conn.ZoneDetails(ctx, zoneID)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	return item, nil
-// }
-
-// func getZoneSettings(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
-// 	conn, err := connect(ctx, d)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	zone := h.Item.(cloudflare.Zone)
-// 	item, err := conn.ZoneSettings(ctx, zone.ID)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	return item.Result, nil
-// }
-
-// func settingsToStandard(ctx context.Context, d *transform.TransformData) (interface{}, error) {
-// 	settings := d.HydrateItem.([]cloudflare.ZoneSetting)
-// 	// Convert the settings into a map, which makes them a lot easier to query by name
-// 	settingsMap := map[string]interface{}{}
-// 	for _, i := range settings {
-// 		settingsMap[i.ID] = i.Value
-// 	}
-// 	return settingsMap, nil
-// }
-
-// func getZoneDNSSEC(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
-// 	conn, err := connect(ctx, d)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	zone := h.Item.(cloudflare.Zone)
-// 	item, err := conn.ZoneDNSSECSetting(ctx, zone.ID)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	return item, nil
-// }
-
-// func getZoneUniversalSSLSettings(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
-// 	conn, err := connect(ctx, d)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	zone := h.Item.(cloudflare.Zone)
-// 	item, err := conn.UniversalSSLSettingDetails(ctx, zone.ID)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	return item, nil
-// }
