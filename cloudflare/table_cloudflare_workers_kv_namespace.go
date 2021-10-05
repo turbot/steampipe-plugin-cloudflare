2ee7629b14dea679ed34121f23cf0cf18be81d13
LalitTurbot <lalit@turbot.com>
2021-10-05 16:40:21 +0530

    update

--- cloudflare/table_cloudflare_workers_kv_namespace.go
+++ cloudflare/table_cloudflare_workers_kv_namespace.go
@@ -17,7 +17,6 @@ func tableCloudflareWorkersKVNamespace(ctx context.Context) *plugin.Table {
 		Columns: []*plugin.Column{
 			{Name: "id", Type: proto.ColumnType_STRING, Description: "Namespace identifier."},
 			{Name: "title", Type: proto.ColumnType_STRING, Description: "A human-readable string name for a Namespace."},
-			// {Name: "keys", Type: proto.ColumnType_JSON, Transform: transform.FromField("Result"), Hydrate: listWorkersKVNamespaceKeys, Description: "A human-readable string name for a Namespace."},
 		},
 	}
 }
@@ -40,23 +39,3 @@ func listWorkersKVNamespaces(ctx context.Context, d *plugin.QueryData, _ *plugin
 	}
 	return nil, nil
 }
-
-//// HYDRATE FUNCTIONS
-// func listWorkersKVNamespaceKeys(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
-// 	logger := plugin.Logger(ctx)
-// 	namespace := h.Item.(cloudflare.WorkersKVNamespace).ID
-
-// 	conn, err := connect(ctx, d)
-// 	if err != nil {
-// 		logger.Error("listWorkersKVNamespaceKeys", "connection error", err)
-// 		return nil, err
-// 	}
-// 	TODO check for paging
-// 	resp, err := conn.ListWorkersKVs(ctx, namespace)
-// 	if err != nil {
-// 		logger.Error("listWorkersKVNamespaceKeys", "api error", err)
-// 		return nil, err
-// 	}
-
-// 	return resp, nil
-// }
