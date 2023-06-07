1277286417fbc28ade45585f50d701b919a65247
Karan Popat <karan@turbot.com>
2023-06-07 21:45:15 +0530

    fix time filter

--- cloudflare/table_cloudflare_user_audit_log.go
+++ cloudflare/table_cloudflare_user_audit_log.go
@@ -2,6 +2,7 @@ package cloudflare
 
 import (
 	"context"
+	"time"
 
 	"github.com/cloudflare/cloudflare-go"
 
@@ -19,7 +20,7 @@ func tableCloudflareUserAuditLog(ctx context.Context) *plugin.Table {
 			KeyColumns: []*plugin.KeyColumn{
 				{
 					Name:       "when",
-					Operators:  []string{">", ">=", "<"},
+					Operators:  []string{">", ">=", "<", "="},
 					Require:    plugin.Optional,
 					CacheMatch: "exact",
 				},
@@ -142,12 +143,16 @@ func listUserAuditLogs(ctx context.Context, d *plugin.QueryData, _ *plugin.Hydra
 	}
 	if d.Quals["when"] != nil {
 		for _, q := range d.Quals["when"].Quals {
-			timestamp := q.Value.GetStringValue()
+			timestamp := q.Value.GetTimestampValue().AsTime().Format(time.RFC3339)
+			timestampBefore := q.Value.GetTimestampValue().AsTime().Add(time.Second).Format(time.RFC3339)
 			switch q.Operator {
 			case ">=", ">":
 				opts.Since = timestamp
 			case "<":
 				opts.Before = timestamp
+			case "=":
+				opts.Since = timestamp
+				opts.Before = timestampBefore
 			}
 		}
 	}
